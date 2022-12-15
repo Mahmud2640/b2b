70b1a7dcdb56d53a0e1af237c8c3f051941df166
Mahmud2640 <mdmahmud484@gmail.com>
2022-12-16 01:41:24 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Logout from "./Pages/Logout/Logout";
 import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="logout" element={<Logout />} />
       </Routes>
     </div>
   );
